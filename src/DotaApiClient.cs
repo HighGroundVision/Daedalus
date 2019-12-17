5a2006d77cd7e58442266df7b5a251af6f9bc68b
Jamie Webster <jamie.webster@factorybucket.com>
2019-12-17 15:40:01 -0500

    Updated the constructor to get ready for DI

--- src/DotaApiClient.cs
+++ src/DotaApiClient.cs
@@ -1,31 +1,22 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace HGV.Daedalus
 {
-	public class DotaApiClient : IDisposable
+	public class DotaApiClient
 	{
-		private string SteamApiKey { get; set; }
+		private readonly string key;
 
-		private HttpClient Client { get; set; }
+		private readonly HttpClient client;
 
-		public DotaApiClient(string key)
+		public DotaApiClient(ISteamKeyProvider provider, IHttpClientFactory httpClientFactory)
 		{
-			if (string.IsNullOrWhiteSpace(key))
-				throw new ArgumentNullException(nameof(key));
-
-			this.SteamApiKey = key;
-
-			this.Client = new HttpClient();
-		}
-
-		public void Dispose()
-		{
-			this.Client.Dispose();
+			this.key = provider.GetKey();
+			this.client = httpClientFactory.CreateClient();
 		}
 
 		/// <summary>
@@ -33,8 +24,8 @@ namespace HGV.Daedalus
 		/// </summary>
 		public async Task<List<GetLeagueListing.League>> GetLeagueListing(string lang = "en")
 		{
-			var url = string.Format("http://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v1/?key={0}&language={1}", this.SteamApiKey, lang);
-			var json = await this.Client.GetStringAsync(url);
+			var url = string.Format("http://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v1/?key={0}&language={1}", this.key, lang);
+			var json = await this.client.GetStringAsync(url);
 			var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetLeagueListing.GetLeagueListingResult>(json);
 			return data?.result?.leagues ?? new List<GetLeagueListing.League>();
 		}
@@ -44,8 +35,8 @@ namespace HGV.Daedalus
 		/// </summary>
 		public async Task<List<GetLiveLeagueGames.Match>> GetLiveLeagueGames()
 		{
-			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v0001?key={0}", this.SteamApiKey);
-			var json = await this.Client.GetStringAsync(url);
+			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v0001?key={0}", this.key);
+			var json = await this.client.GetStringAsync(url);
 			var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetLiveLeagueGames.GetLiveLeagueGamesResult>(json);
 			return data?.result?.games ?? new List<GetLiveLeagueGames.Match>();
 		}
@@ -58,8 +49,8 @@ namespace HGV.Daedalus
 			if (matchId == 0)
 				throw new ArgumentOutOfRangeException(nameof(matchId));
 
-			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v0001?key={0}&match_id={1}&include_persona_names=1", this.SteamApiKey, matchId);
-			var json = await this.Client.GetStringAsync(url);
+			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v0001?key={0}&match_id={1}&include_persona_names=1", this.key, matchId);
+			var json = await this.client.GetStringAsync(url);
 			var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetMatchDetails.GetMatchDetailsResult>(json);
 			return data?.result;
 		}
@@ -69,8 +60,8 @@ namespace HGV.Daedalus
 		/// </summary>
 		public async Task<List<GetMatchHistory.Match>> GetLastestMatches()
         {
-            var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key={0}", this.SteamApiKey);
-            var json = await this.Client.GetStringAsync(url);
+            var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key={0}", this.key);
+            var json = await this.client.GetStringAsync(url);
             var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetMatchHistory.GetMatchHistoryResult>(json);
             return data?.result?.matches ?? new List<Daedalus.GetMatchHistory.Match>();
         }
@@ -83,8 +74,8 @@ namespace HGV.Daedalus
             if (number == 0)
                 throw new ArgumentOutOfRangeException(nameof(number));
 
-            var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/V001/?key={0}&start_at_match_seq_num={1}", this.SteamApiKey, number);
-            var json = await this.Client.GetStringAsync(url);
+            var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/V001/?key={0}&start_at_match_seq_num={1}", this.key, number);
+            var json = await this.client.GetStringAsync(url);
             var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetMatchHistoryBySequenceNum.GetMatchHistoryBySequenceNumResult>(json);
             return data?.result?.matches ?? new List<Daedalus.GetMatchDetails.Match>();
         }
@@ -97,8 +88,8 @@ namespace HGV.Daedalus
 			if (accountId == 0)
 				throw new ArgumentOutOfRangeException(nameof(accountId));
 
-			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key={0}&account_id={1}", this.SteamApiKey, accountId);
-			var json = await this.Client.GetStringAsync(url);
+			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key={0}&account_id={1}", this.key, accountId);
+			var json = await this.client.GetStringAsync(url);
 			var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetMatchHistory.GetMatchHistoryResult>(json);
 			return data?.result?.matches ?? new List<Daedalus.GetMatchHistory.Match>();
 		}
@@ -114,11 +105,8 @@ namespace HGV.Daedalus
 			if (heroId == 0)
 				throw new ArgumentOutOfRangeException(nameof(heroId));
 
-			if(heroId > 120)
-				throw new ArgumentOutOfRangeException(nameof(heroId));
-
-			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key={0}&account_id={1}&hero_id={2}", this.SteamApiKey, accountId, heroId);
-			var json = await this.Client.GetStringAsync(url);
+			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key={0}&account_id={1}&hero_id={2}", this.key, accountId, heroId);
+			var json = await this.client.GetStringAsync(url);
 			var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetMatchHistory.GetMatchHistoryResult>(json);
 			return data?.result?.matches ?? new List<Daedalus.GetMatchHistory.Match>();
 		}
@@ -128,8 +116,8 @@ namespace HGV.Daedalus
 		/// </summary>
 		public async Task<string> GetScheduledLeagueGames()
 		{
-			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetScheduledLeagueGames/v0001/?key={0}", this.SteamApiKey);
-			var json = await this.Client.GetStringAsync(url);
+			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetScheduledLeagueGames/v0001/?key={0}", this.key);
+			var json = await this.client.GetStringAsync(url);
 			return json;
 		}
 		
@@ -138,8 +126,8 @@ namespace HGV.Daedalus
 		/// </summary>
 		public async Task<GetTeamInfoByTeamID.Team> GetTeamInfoByTeamID(long teamId)
 		{
-			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetTeamInfoByTeamID/v0001/?key={0}&start_at_team_id={1}&teams_requested=1", this.SteamApiKey, teamId);
-			var json = await this.Client.GetStringAsync(url);
+			var url = string.Format("https://api.steampowered.com/IDOTA2Match_570/GetTeamInfoByTeamID/v0001/?key={0}&start_at_team_id={1}&teams_requested=1", this.key, teamId);
+			var json = await this.client.GetStringAsync(url);
 			var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetTeamInfoByTeamID.GetTeamInfoByTeamIDResult>(json);
 			return data?.result?.teams?.FirstOrDefault();
 		}
@@ -149,8 +137,8 @@ namespace HGV.Daedalus
 		/// </summary>
 		public async Task<GetPlayerSummaries.Player> GetPlayerSummary(long steamId)
 		{
-			var url = string.Format("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={0}&steamids={1}", this.SteamApiKey, steamId);
-			var json = await this.Client.GetStringAsync(url);
+			var url = string.Format("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={0}&steamids={1}", this.key, steamId);
+			var json = await this.client.GetStringAsync(url);
 			var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetPlayerSummaries.GetPlayerSummariesResult>(json);
 			var player = data?.response?.players?.FirstOrDefault();
 			return player;
@@ -164,8 +152,8 @@ namespace HGV.Daedalus
             if (ids.Count > 100)
                 throw new ArgumentOutOfRangeException("ids", ids.Count, "Can only process 100 profiles at a time.");
 
-            var url = string.Format("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={0}&steamids={1}", this.SteamApiKey, String.Join(",", ids.ToArray()));
-            var json = await this.Client.GetStringAsync(url);
+            var url = string.Format("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={0}&steamids={1}", this.key, String.Join(",", ids.ToArray()));
+            var json = await this.client.GetStringAsync(url);
             var data = Newtonsoft.Json.JsonConvert.DeserializeObject<GetPlayerSummaries.GetPlayerSummariesResult>(json);
             return data?.response?.players;
         }
@@ -175,8 +163,8 @@ namespace HGV.Daedalus
         /// </summary>
         public async Task<string> GetHeroes()
 		{
-			var url = string.Format("https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1/?key={0}", this.SteamApiKey);
-			var json = await this.Client.GetStringAsync(url);
+			var url = string.Format("https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1/?key={0}", this.key);
+			var json = await this.client.GetStringAsync(url);
 			return json;
 		}
 	}
